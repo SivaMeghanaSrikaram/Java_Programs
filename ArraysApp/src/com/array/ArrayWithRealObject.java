ebd9da636666759c0334152efe356b96d82ee640
Meghana <228A1A05A1@risekrishnasaiprakasam.edu.in>
2025-08-04 09:07:40 +0530

    Add files via upload

---
+++ ArraysApp/src/com/array/ArrayWithRealObject.java
@@ -0,0 +1,30 @@
+package com.array;
+
+public class ArrayWithRealObject {
+
+	public static void main(String[] args) {
+		
+		//array with object(product)
+		Product[] products = new Product[3];
+		
+		//create product object
+		Product p = new Product(1001,"LG", 20000);
+		Product p1 = new Product(1002,"Realme", 15000);
+		Product p2 = new Product(1003,"Samsung", 12000);
+		
+		//insert
+		products[0]=p;
+		products[1]=p1;
+		products[2]=p2;
+		
+		//Display- enhanced for loop
+		
+		for(Product pro:products) {
+			System.out.println(pro.getPid()+" "+
+		                       pro.getPname()+" "+
+					           pro.getPrice());
+		}
+		
+	}
+
+}
